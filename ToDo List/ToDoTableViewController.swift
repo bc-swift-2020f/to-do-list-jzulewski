c621903abd7b1ccff4a054cdf0827a68544378b3
John Zulewski <zulewskj@bc.edu>
2020-09-28 08:35:53 -0400

    5.4

--- ToDo List/ToDoTableViewController.swift
+++ ToDo List/ToDoTableViewController.swift
@@ -13,10 +13,22 @@ class ToDoTableViewController: UITableViewController {
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var noteView: UITextView!
+    
+    var toDoItem: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        if toDoItem == nil {
+            toDoItem = ""
+        }
+        
+        nameField.text = toDoItem
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        toDoItem = nameField.text
     }
 
     @IBAction func cancelButtonPressed(_ sender: Any) {
