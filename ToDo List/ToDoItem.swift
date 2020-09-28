afd48aaf5c1a0a1de33e61c23e511d18cd5c26b0
John Zulewski <zulewskj@bc.edu>
2020-09-28 10:32:24 -0400

    5.6

---
+++ ToDo List/ToDoItem.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoItem.swift
+//  ToDo List
+//
+//  Created by John Zulewski on 9/28/20.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
